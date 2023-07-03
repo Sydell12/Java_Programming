8e7ab2f04887b674cf5aa604002e4bf285ad95a4
Sydell12 <sydellinstar@gmail.com>
2023-07-03 17:37:54 +0700

    Chapter1
    Ex of chapter1

---
+++ Chapter1/DisplaysArea.java
@@ -0,0 +1,8 @@
+public class DisplaysArea {     //this code for Exercise 1.3
+    public static void main(String[] args) {
+        System.out.print( "Area = ");
+        System.out.println(4.5 * 7.9);
+        System.out.print( "Perimeter = ");
+        System.out.println((4.5 + 7.9)*2);
+    }
+}
---
+++ Chapter1/PopulationProjection.java
@@ -0,0 +1,14 @@
+public class PopulationProjection {         //this code for Exercise 1.4
+    public static void main(String[] args) {
+        System.out.print("In 1 years the population will be =        ");
+        System.out.println(312032486 + ((365 * 24 * 60 * 60) / 7) - ((365 * 24 * 60 * 60) / 13) + ((365 * 24 * 60 * 60) / 45));
+        System.out.print("In 2 years the population will be =        ");
+        System.out.println(312032486 + ((365 * 24 * 60 * 60) * 2 / 7) - ((365 * 24 * 60 * 60) * 2/ 13) + ((365 * 24 * 60 * 60) * 2 / 45));
+        System.out.print("In 3 years the population will be =        ");
+        System.out.println(312032486 + ((365 * 24 * 60 * 60) * 3 / 7) - ((365 * 24 * 60 * 60) * 3 / 13) + ((365 * 24 * 60 * 60) * 3 / 45));
+        System.out.print("In 4 years the population will be =        ");
+        System.out.println(312032486 + ((365 * 24 * 60 * 60) * 4 / 7) - ((365 * 24 * 60 * 60) * 4 / 13) + ((365 * 24 * 60 * 60) * 4 / 45));
+        System.out.print("In 5 years the population will be =        ");
+        System.out.println(312032486 + ((365 * 24 * 60 * 60) * 5 / 7) - ((365 * 24 * 60 * 60) * 5 / 13) + ((365 * 24 * 60 * 60) * 5 / 45));
+    }
+}
